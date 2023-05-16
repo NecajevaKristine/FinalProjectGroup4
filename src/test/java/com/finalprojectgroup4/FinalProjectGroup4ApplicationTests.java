c88893df70031b7f590681a8dbf0e8a5d8acf8a0
Kristine Necajeva <kristinenecajeva@inbox.lv>
2023-05-16 22:14:58 +0300

    First commit

---
+++ src/test/java/com/finalprojectgroup4/FinalProjectGroup4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalprojectgroup4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectGroup4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
